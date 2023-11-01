9d196b2080f2aaeeda102e1df573fe7da6a71731
Ban Sara Rebeca <125962887+BanSaraRbk@users.noreply.github.com>
2023-11-01 10:54:51 +0200

    Update fifo_new_constraints.vhdl

--- fifo_new_constraints.vhdl
+++ fifo_new_constraints.vhdl
@@ -35,6 +35,7 @@ entity fifoo is
     dac_wrt_o: out  std_logic ;
     dac_rst_o:out  std_logic ;
     dac_clk_o: out std_logic ;
+   
     dac_sel_o:out  std_logic;
     moove        :out std_logic_vector(DATA_WIDTH-1 downto 0) 
    );
@@ -42,14 +43,15 @@ entity fifoo is
 end fifoo;
 
 architecture Behavioral of fifoo is
+    signal rst        :  std_logic;
     signal clk:std_logic;
     signal empty        :std_logic;
     signal full :  std_logic;
-    signal rst        : std_logic;
     signal fifo_data: STD_LOGIC_VECTOR(DATA_WIDTH-1 downto 0);
-    signal cnt: integer:=0;
+    signal  cnt: integer:=0;
     signal wr_en:std_logic;
     signal rd_en:std_logic;
+    signal adc_clk_in :std_logic;
  
         signal moving_avg_good: STD_LOGIC_VECTOR(DATA_WIDTH-1 downto 0);
 
@@ -85,66 +87,41 @@ fifo1 :fifo_generator_0
 
    
   );
-ex :IBUFDS
-port map (
-  I  => adc_clk_p_i,
-  IB => adc_clk_n_i,
-  O  => clk
-);
-  clk_oddr : ODDR
-      port map (
-        Q => dac_clk_o,
-        D1 =>'0',
-        D2 => '1',
-        C => clk,
-        CE => '1',
-        R => '0',
-        S => '0'
-      );
-    sel_oddr : ODDR
-      port map (
-        Q => dac_sel_o,
-        D1 =>'0',
-        D2 => '1',
-        C => clk,
-        CE => '1',
-        R => '0',
-        S => '0'
-      );
-  wrt_oddr : ODDR
-      port map (
-        Q => dac_wrt_o,
-        D1 =>'1',
-        D2 => '1',
-        C => clk,
-        CE => '1',
-        R => '0',
-        S => '0'
-      );
-     rst_oddr : ODDR
-      port map (
-        Q => dac_rst_o,
-        D1 =>'0',
-        D2 => '0',
-        C => clk,
-        CE => '1',
-        R => '0',
-        S => '0'
-      );
+ex :IBUFDS port map (  I  => adc_clk_p_i, IB => adc_clk_n_i,O  => clk);
+
+buf:  BUFG port map( O=>  clk, I=> adc_clk_in);
+             
+SRL16E_inst : SRL16
+generic map (
+   INIT => X"1111")
+port map ( Q => rst,   A0 => '0',   A1 => '1',  A2 => '0',  A3 => '0',   CLK => clk,    D => '0' );
+
+
+
+clk_oddr : ODDR  port map(Q => dac_clk_o, D1 => '0', D2 => '1', C => clk, CE => '1', R => '0', S => '0');
+sel_oddr : ODDR  port map(Q => dac_sel_o, D1 => '0', D2 => '1', C => clk, CE => '1', R => '0', S => '0');
+wrt_oddr : ODDR port map (Q => dac_wrt_o, D1 => '1', D2 => '1', C => clk, CE => '1', R => '0', S => '0');
+rst_oddr : ODDR port map (Q => dac_rst_o, D1 => '0', D2 => '0', C => clk, CE => '1', R => '0', S => '0');
+
+
+
+
+
+
 GEN:
     for j in 0 to 13 generate
       DAC_DAT_inst : ODDR
       port map (
         Q => dac_dat_o(j),
-        D1 => adc_dat_a_i(j),
-        D2 => moving_avg_good(j),
+        D1 => moving_avg_good(j),
+        D2 => adc_dat_a_i(j),
         C => clk,
         CE => '1',
         R => '0',
         S => '0'
       );
- 
   end generate GEN;
+ 
   
 --  clk<=adc_clk_p_i and adc_clk_n_i;
   
@@ -163,9 +140,11 @@ GEN:
                 rd_en<='0';
 --                dac_dat_o<=(others=>'0');
                 moving_avg:=(others=>'0');
+                moving_avg_good<=(others=>'0');
+                moove<=(others=>'0');
                 
                cnt <= 2 ** param(3) - 2;
-                
+--                cnt <=6;
                   
             elsif rising_edge(clk) then
             
@@ -190,8 +169,7 @@ GEN:
           
            moving_avg := std_logic_vector(shift_right (signed (sum), param(3)));
            moving_avg_good <=moving_avg(13 downto 0);
-            
-                moove<=moving_avg_good;
+           moove<=moving_avg_good;
           
       
             
@@ -207,5 +185,4 @@ GEN:
 
 
 end Behavioral;
-
 
